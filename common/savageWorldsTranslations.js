d57d492507d21ff619645dfd126d8567ca4377e0
DarkLord <dfhrhfan12@yandex.ru>
2024-08-27 23:37:23 +0400

    Add base translations

---
+++ common/savageWorldsTranslations.js
@@ -0,0 +1,52 @@
+const { WordCaseForm, WordCountForm, TranslationsManager } = require('../common/translationLib');
+
+const StatesTranslations = (function () {
+    const res = new TranslationsManager();
+
+    //
+
+    return res;
+})();
+
+const SkillsTranslations = (function () {
+    const res = new TranslationsManager();
+
+    //
+
+    return res;
+})();
+
+const HindrancesTranslations = (function () {
+    const res = new TranslationsManager();
+
+    //
+
+    return res;
+})();
+
+const EdgesTranslations = (function () {
+    const res = new TranslationsManager();
+
+    //
+
+    return res;
+})();
+
+const CreaturesTranslations = (function () {
+    const res = new TranslationsManager();
+
+    //
+
+    return res;
+})();
+
+
+module.exports = {
+    WordCountForm,
+    WordCaseForm,
+    StatesTranslations,
+    SkillsTranslations,
+    HindrancesTranslations,
+    EdgesTranslations,
+    CreaturesTranslations,
+};
