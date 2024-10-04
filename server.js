6d93ad3e581ed3570d9526786202ed3910cb5e25
DarkLord <dfhrhfan12@yandex.ru>
2024-10-04 23:08:04 +0400

    Add index page

--- server.js
+++ server.js
@@ -3,10 +3,18 @@ const http = require('http');
 const fs = require("fs");
 
 http.createServer((request, response) => {
-    const { fonts, docDefinition } = require('./SavegedWarcraftRu/content');
+    if (request.url == '/SavegedWarcraftRu') {
+        const { fonts, docDefinition } = require('./SavegedWarcraftRu/content');
 
-    const printer = new PdfPrinter(fonts);
-    var pdfDoc = printer.createPdfKitDocument(docDefinition);
-    pdfDoc.pipe(response);
-    pdfDoc.end();
+        const printer = new PdfPrinter(fonts);
+        var pdfDoc = printer.createPdfKitDocument(docDefinition);
+        pdfDoc.pipe(response);
+        pdfDoc.end();
+
+        return;
+    }
+
+    // res.writeHead(404, { 'Content-Type': 'text/plain' });
+    // res.end(`No content: ${req.url}`);
+    fs.createReadStream(`${__dirname}/index.html`).pipe(response);
 }).listen(8080);
