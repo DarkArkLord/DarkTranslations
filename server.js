e2f24a307a89242dd2f477658789d2edd14f75be
DarkLord <dfhrhfan12@yandex.ru>
2024-10-05 20:43:06 +0400

    Remove index

--- server.js
+++ server.js
@@ -3,18 +3,10 @@ const http = require('http');
 const fs = require("fs");
 
 http.createServer((request, response) => {
-    if (request.url == '/SavegedWarcraftRu') {
-        const { fonts, docDefinition } = require('./SavegedWarcraftRu/content');
+    const { fonts, docDefinition } = require('./SavegedWarcraftRu/content');
 
-        const printer = new PdfPrinter(fonts);
-        var pdfDoc = printer.createPdfKitDocument(docDefinition);
-        pdfDoc.pipe(response);
-        pdfDoc.end();
-
-        return;
-    }
-
-    // res.writeHead(404, { 'Content-Type': 'text/plain' });
-    // res.end(`No content: ${req.url}`);
-    fs.createReadStream(`${__dirname}/index.html`).pipe(response);
+    const printer = new PdfPrinter(fonts);
+    var pdfDoc = printer.createPdfKitDocument(docDefinition);
+    pdfDoc.pipe(response);
+    pdfDoc.end();
 }).listen(8080);
